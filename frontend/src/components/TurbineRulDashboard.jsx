3d99473893538e5d108d9148fc75924353ede5e8
pareshnaikiitj <m25de2039@iitj.ac.in>
2026-08-23 15:55:19 +0530

    updated: plots

--- frontend/src/components/TurbineRulDashboard.jsx
+++ frontend/src/components/TurbineRulDashboard.jsx
@@ -4,7 +4,7 @@ import { useModelMetrics } from '../hooks/useModelMetrics';
 import { useCsvRulPrediction } from '../hooks/useCsvRulPrediction';
 import SimulationControls from './SimulationControls';
 import SimulationResults from './SimulationResults';
-import CsvUploadControls from './CsvUploadControls';
+import CsvUploadControls from './CsvUploadcontrols';
 import CsvUploadPredictions from './CsvUploadPredictions';
 import ParameterTrends from './ParameterTrends';
 
